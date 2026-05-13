bf1a6647438c7e4b087ed076e1cf08a7f14295a8
Rafael Manjarres <rafaelmanjarres1995@gmail.com>
2026-05-13 15:07:53 -0500

    Implemente Retry para que haga reintento por si algo en la app falla.

--- gestion-estudiantes/src/main/java/com/estudiantes/gestion_estudiantes/application/usecase/StudentMessageUseCase.java
+++ gestion-estudiantes/src/main/java/com/estudiantes/gestion_estudiantes/application/usecase/StudentMessageUseCase.java
@@ -6,6 +6,8 @@ import jakarta.validation.ConstraintViolation;
 import jakarta.validation.Validator;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -16,6 +18,12 @@ public class StudentMessageUseCase implements StudentMessageInputPort {
     private final StudentInputPort studentInputPort;
     private final Validator validator;
     @Override
+    @Retryable(
+        retryFor = Exception.class,
+        exclude = IllegalArgumentException.class,
+        maxAttempts = 3,
+        backoff = @Backoff(delay = 2000, multiplier = 2)
+    )
     public void processStudentMessage(StudentRequestDTO dto) {
         log.info("[SQS] Procesando mensaje para estudiante: {} {}", dto.getName(), dto.getLastNames());
         Set<ConstraintViolation<StudentRequestDTO>> violations = validator.validate(dto);
@@ -23,7 +31,7 @@ public class StudentMessageUseCase implements StudentMessageInputPort {
             String errors = violations.stream()
                     .map(ConstraintViolation::getMessage)
                     .collect(Collectors.joining(", "));
-            log.error("[SQS] Mensaje invalido, sera enviado a DLQ. Errores: {}", errors);
+            log.error("[SQS] Mensaje invalido, no se reintenta. Errores: {}", errors);
             throw new IllegalArgumentException("Mensaje SQS invalido: " + errors);
         }
         studentInputPort.createStudent(dto);
